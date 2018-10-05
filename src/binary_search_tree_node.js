a8fd74ad68ce27f698375b23fb3dff7d93501f80
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2018-10-05 19:47:42 +0600

    added removing in binary search tree node

--- src/binary_search_tree_node.js
+++ src/binary_search_tree_node.js
@@ -49,8 +49,42 @@ BinarySearchTreeNode.prototype.find = function(val) {
   return findInternal(this);
 }
 
-BinarySearchTreeNode.prototype.remove = function(val) {
-  
+BinarySearchTreeNode.prototype.remove = function(val, withConsole) {
+  var self = this;
+  function removeInner(node, value, withConsoleLog) {
+    val = value || val;
+    if (node === null || node === undefined) {
+      return node;
+    } else if (node.value > val) {
+      node.left = removeInner(node.left);
+    } else if (node.value < val) {
+      node.right = removeInner(node.right);
+    } else {
+      if (node.right === null && node.left === null) {
+        node = null;
+      } else if (node.left === null) {
+        node = node.right;
+      } else if (node.right === null) {
+        node = node.left;
+      } else {
+        var temp = node.right.findMin();
+        node.value = temp.value;
+        node.right = removeInner(node.right, node.value, true);
+      }
+    }
+
+    return node;
+  }
+
+  var res = removeInner(this);
+
+  if ((res === null) || (res.left === null && res.right === null)) {
+    this.value = res ? res.value : null;
+    this.left = null;
+    this.right = null;
+  }
+
+  return res ? true : false;
 }
 
 BinarySearchTreeNode.prototype.findMin = function() {
--- test/binary_search_tree_node.spec.js
+++ test/binary_search_tree_node.spec.js
@@ -103,7 +103,7 @@ describe('BinarySearchTreeNode', function (){
     expect(node.find(30).meta.get('color')).toBe('black');
   });
 
-  fit('should find node', function (){
+  it('should find node', function (){
     const node = new BinarySearchTreeNode(10);
 
     node.insert(20);
@@ -117,7 +117,7 @@ describe('BinarySearchTreeNode', function (){
     expect(node.find(5).value).toBe(5);
   });
 
-  fit('should remove leaf nodes', function (){
+  it('should remove leaf nodes', function (){
     const bstRootNode = new BinarySearchTreeNode();
 
     bstRootNode.insert(10);
@@ -134,87 +134,87 @@ describe('BinarySearchTreeNode', function (){
     expect(bstRootNode.toString()).toBe('10');
     expect(removed2).toBe(true);
   });
-  //
-  // it('should remove nodes with one child', function (){
-  //   const bstRootNode = new BinarySearchTreeNode();
-  //
-  //   bstRootNode.insert(10);
-  //   bstRootNode.insert(20);
-  //   bstRootNode.insert(5);
-  //   bstRootNode.insert(30);
-  //
-  //   expect(bstRootNode.toString()).toBe('5,10,20,30');
-  //
-  //   bstRootNode.remove(20);
-  //   expect(bstRootNode.toString()).toBe('5,10,30');
-  //
-  //   bstRootNode.insert(1);
-  //   expect(bstRootNode.toString()).toBe('1,5,10,30');
-  //
-  //   bstRootNode.remove(5);
-  //   expect(bstRootNode.toString()).toBe('1,10,30');
-  // });
-  //
-  // it('should remove nodes with two children', function (){
-  //   const bstRootNode = new BinarySearchTreeNode();
-  //
-  //   bstRootNode.insert(10);
-  //   bstRootNode.insert(20);
-  //   bstRootNode.insert(5);
-  //   bstRootNode.insert(30);
-  //   bstRootNode.insert(15);
-  //   bstRootNode.insert(25);
-  //
-  //   expect(bstRootNode.toString()).toBe('5,10,15,20,25,30');
-  //   expect(bstRootNode.find(20).left.value).toBe(15);
-  //   expect(bstRootNode.find(20).right.value).toBe(30);
-  //
-  //   bstRootNode.remove(20);
-  //   expect(bstRootNode.toString()).toBe('5,10,15,25,30');
-  //
-  //   bstRootNode.remove(15);
-  //   expect(bstRootNode.toString()).toBe('5,10,25,30');
-  //
-  //   bstRootNode.remove(10);
-  //   expect(bstRootNode.toString()).toBe('5,25,30');
-  //   expect(bstRootNode.value).toBe(25);
-  //
-  //   bstRootNode.remove(25);
-  //   expect(bstRootNode.toString()).toBe('5,30');
-  //
-  //   bstRootNode.remove(5);
-  //   expect(bstRootNode.toString()).toBe('30');
-  // });
-  //
-  // it('should remove node with no parent', function (){
-  //   const bstRootNode = new BinarySearchTreeNode();
-  //   expect(bstRootNode.toString()).toBe('');
-  //
-  //   bstRootNode.insert(1);
-  //   bstRootNode.insert(2);
-  //   expect(bstRootNode.toString()).toBe('1,2');
-  //
-  //   bstRootNode.remove(1);
-  //   expect(bstRootNode.toString()).toBe('2');
-  //
-  //   bstRootNode.remove(2);
-  //   expect(bstRootNode.toString()).toBe('');
-  // });
-  //
-  // it('should throw error when trying to remove not existing node', function (){
-  //   const bstRootNode = new BinarySearchTreeNode();
-  //
-  //   bstRootNode.insert(10);
-  //   bstRootNode.insert(20);
-  //
-  //   function removeNotExistingElementFromTree() {
-  //     bstRootNode.remove(30);
-  //   }
-  //
-  //   expect(removeNotExistingElementFromTree).toThrow();
-  // });
-  //
-  // it('should be possible to use objects as node values', function (){
+
+  it('should remove nodes with one child', function (){
+    const bstRootNode = new BinarySearchTreeNode();
+
+    bstRootNode.insert(10);
+    bstRootNode.insert(20);
+    bstRootNode.insert(5);
+    bstRootNode.insert(30);
+
+    expect(bstRootNode.toString()).toBe('5,10,20,30');
+
+    bstRootNode.remove(20);
+    expect(bstRootNode.toString()).toBe('5,10,30');
+
+    bstRootNode.insert(1);
+    expect(bstRootNode.toString()).toBe('1,5,10,30');
+
+    bstRootNode.remove(5);
+    expect(bstRootNode.toString()).toBe('1,10,30');
+  });
+
+  it('should remove nodes with two children', function (){
+    const bstRootNode = new BinarySearchTreeNode();
+
+    bstRootNode.insert(10);
+    bstRootNode.insert(20);
+    bstRootNode.insert(5);
+    bstRootNode.insert(30);
+    bstRootNode.insert(15);
+    bstRootNode.insert(25);
+
+    expect(bstRootNode.toString()).toBe('5,10,15,20,25,30');
+    expect(bstRootNode.find(20).left.value).toBe(15);
+    expect(bstRootNode.find(20).right.value).toBe(30);
+
+    bstRootNode.remove(20);
+    expect(bstRootNode.toString()).toBe('5,10,15,25,30');
+
+    bstRootNode.remove(15);
+    expect(bstRootNode.toString()).toBe('5,10,25,30');
+
+    bstRootNode.remove(10);
+    expect(bstRootNode.toString()).toBe('5,25,30');
+    expect(bstRootNode.value).toBe(25);
+
+    bstRootNode.remove(25);
+    expect(bstRootNode.toString()).toBe('5,30');
+
+    bstRootNode.remove(5);
+    expect(bstRootNode.toString()).toBe('30');
+  });
+
+  it('should remove node with no parent', function (){
+    const bstRootNode = new BinarySearchTreeNode();
+    expect(bstRootNode.toString()).toBe('');
+
+    bstRootNode.insert(1);
+    bstRootNode.insert(2);
+    expect(bstRootNode.toString()).toBe('1,2');
+
+    bstRootNode.remove(1);
+    expect(bstRootNode.toString()).toBe('2');
+
+    bstRootNode.remove(2);
+    expect(bstRootNode.toString()).toBe('');
+  });
+
+  xit('should throw error when trying to remove not existing node', function (){
+    const bstRootNode = new BinarySearchTreeNode();
+
+    bstRootNode.insert(10);
+    bstRootNode.insert(20);
+
+    function removeNotExistingElementFromTree() {
+      bstRootNode.remove(30);
+    }
+
+    expect(removeNotExistingElementFromTree).toThrow();
+  });
+
+  // fit('should be possible to use objects as node values', function (){
   //   const nodeValueComparatorCallback = function(a, b) {
   //     const normalizedA = a || { value: null };
   //     const normalizedB = b || { value: null };
@@ -244,13 +244,13 @@ describe('BinarySearchTreeNode', function (){
   //
   //   expect(bstNode.findMin().value).toEqual(obj1);
   // });
-  //
-  // it('should abandon removed node', function (){
-  //   const rootNode = new BinarySearchTreeNode('foo');
-  //   rootNode.insert('bar');
-  //   const childNode = rootNode.find('bar');
-  //   rootNode.remove('bar');
-  //
-  //   expect(childNode.parent).toBeNull();
-  // });
+
+  xit('should abandon removed node', function (){
+    const rootNode = new BinarySearchTreeNode('foo');
+    rootNode.insert('bar');
+    const childNode = rootNode.find('bar');
+    rootNode.remove('bar');
+
+    expect(childNode.parent).toBeNull();
+  });
 });
