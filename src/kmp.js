7643c11d248e815dd16c48b4b01fcbfb39babca9
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2018-10-12 17:17:41 +0600

    added kmp

---
+++ src/kmp.js
@@ -0,0 +1,43 @@
+function knuthMorrisPratt(s, f) {
+  if (s.length <= 1) {
+    return 0;
+  }
+  var arr = constructMatches(f), j = 0, res = -1;
+  for (var i = 0; i < s.length; i++ ) {
+    if (s[i] === f[j]) {
+      j++;
+
+      if (f.length === j) {
+        res = i - j + 1;
+      }
+    } else {
+      j = arr[j >= 1 ? (j - 1) : 0];
+
+      if (s[i] === f[j]) {
+        j++;
+      }
+    }
+  }
+  return res;
+}
+
+function constructMatches(str) {
+  var res = new Array(str.length), i = 1, j = 0;
+  res[0] = 0;
+  while (i < str.length) {
+    if (str[i] === str[j]) {
+      res[i++] = ++j;
+    } else {
+      j = j >= 1 ? res[j-1] : 0;
+      if (str[i] === str[j]) {
+        res[i++] = ++j;
+      } else {
+        res[i++] = j;
+      }
+    }
+  }
+
+  return res;
+}
+
+module.exports = knuthMorrisPratt;
---
+++ test/kmp.spec.js
@@ -0,0 +1,15 @@
+var knuthMorrisPratt = require('../src/kmp');
+
+describe('knuthMorrisPratt', function() {
+  it('should find word position in given text', function() {
+    expect(knuthMorrisPratt('', '')).toBe(0);
+    expect(knuthMorrisPratt('a', '')).toBe(0);
+    expect(knuthMorrisPratt('a', 'a')).toBe(0);
+    expect(knuthMorrisPratt('abcbcglx', 'abca')).toBe(-1);
+    expect(knuthMorrisPratt('abcbcglx', 'bcgl')).toBe(3);
+    expect(knuthMorrisPratt('abcxabcdabxabcdabcdabcy', 'abcdabcy')).toBe(15);
+    expect(knuthMorrisPratt('abcxabcdabxabcdabcdabcy', 'abcdabca')).toBe(-1);
+    expect(knuthMorrisPratt('abcxabcdabxaabcdabcabcdabcdabcy', 'abcdabca')).toBe(12);
+    expect(knuthMorrisPratt('abcxabcdabxaabaabaaaabcdabcdabcy', 'aabaabaaa')).toBe(11);
+  });
+});
